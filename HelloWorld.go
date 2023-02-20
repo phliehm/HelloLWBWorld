e669958a9c43369e0f0032c789ec1c2ad87034bf
Philipp <philipp.liehm@gmail.com>
2023-02-20 12:03:07 +0100

    Check this out man

--- HelloWorld.go
+++ HelloWorld.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+func PrintAny(a string) {
+	fmt.Println(a,b)
+	
+}
+
 func main() {
 	fmt.Println("Hello World!\n",laberphil)
+	PrintAny("Das")
 }
 
 var laberphil string = "Jetzt hab ich nen bug rein gemacht" + "HAHAAHAHAHAHA"
+var b string  = "Geht ni"
+
 // Voll kreativ!
 // Weil ich krass bin!!
